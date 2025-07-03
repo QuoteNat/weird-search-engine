e703f3446f6a3048c367784bec9cafa12f9141c2
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2025-07-03 13:37:41 -0500

    Add link parsing

--- spider/main.py
+++ spider/main.py
@@ -1,3 +1,15 @@
+from bs4 import BeautifulSoup
+
+def parse_page(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    parse = {"links": [],
+             "title": "",
+             "words": []}
+    for link in soup.find_all('a'):
+        parse["links"].append(link.get('href'))
+        
+    return parse
+
 def main():
     print("Hello from spider!")
 
