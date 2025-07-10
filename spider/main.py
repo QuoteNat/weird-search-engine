64ec96088b870c20a4bf0f54b73975d4c46e8105
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2025-07-10 15:11:24 -0500

    Add initial celery task

--- spider/main.py
+++ spider/main.py
@@ -1,7 +1,17 @@
 from bs4 import BeautifulSoup
 import re
+from celery import Celery
+import logging
+import urllib.request
 
-def parse_page(html):
+app = Celery('scraper', broker='redis://localhost:6379/0')
+logger = logging.getLogger(__name__)
+
+@app.task
+def parse_page(url):
+    html = None
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
     soup = BeautifulSoup(html, 'html.parser')
     parse = {"links": [],
              "title": "",
@@ -11,11 +21,12 @@ def parse_page(html):
     parse["title"] = soup.title.string
     unfiltered_text = soup.get_text()
     parse["words"] = re.sub("[^\\w]", " ", unfiltered_text).split()
+    logging.info(parse)
     return parse
 
-def main():
-    print("Hello from spider!")
-
-
 if __name__ == "__main__":
-    main()
+    app.start()
+    urls = ["https://wikipedia.com"]
+    for url in urls:
+        parse_page.delay(url)    
+
