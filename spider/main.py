6f2c130fae5595f14b9ea32437ee40e7b2322004
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2025-07-11 10:15:59 -0500

    Fix rate limit config call

--- spider/main.py
+++ spider/main.py
@@ -2,8 +2,8 @@ from tasks import parse_page, app
 
 if __name__ == "__main__":
     # Seed urls for the scraper to work with
-    urls = ["https://wikipedia.com"]
-    app.control.rate_limit('myapp.parse_page', '60/m')
+    urls = ["https://wikipedia.com", "https://npr.org", "https://www.allaboutbirds.org/news/"]
+    app.control.rate_limit('tasks.parse_page', '60/m')
     for url in urls:
         parse_page.delay(url)    
 
