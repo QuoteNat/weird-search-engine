44c051b66497466b30ab128e49c317bb5df7a7e1
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2025-08-07 14:50:05 -0500

    Fetch feeds based on search term tokens

--- weird-search-frontend/app/controllers/search_controller.rb
+++ weird-search-frontend/app/controllers/search_controller.rb
@@ -1,5 +1,8 @@
 class SearchController < ApplicationController
   def get
-    
+    @results = []
+    params[:query].to_s.split(/\W+/) do |token|
+      @results += Page.where("? = ANY(words)", token)
+    end
   end
 end
