e7773268879385e6146e6187c3750ad13fa08224
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2025-08-11 15:54:58 -0500

    Remove duplicate results

--- weird-search-frontend/app/controllers/search_controller.rb
+++ weird-search-frontend/app/controllers/search_controller.rb
@@ -2,8 +2,7 @@ class SearchController < ApplicationController
   def get
     @results = []
     params[:query].to_s.split(/\W+/) do |token|
-      @results += Page.where("? = ANY(title_tokens)", token)
-      @results += Page.where("? = ANY(word_tokens)", token)
+      @results += Page.where("? = ANY(title_tokens)", token).or(Page.where("? = ANY(word_tokens)", token))
     end
   end
 end
